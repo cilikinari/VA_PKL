d3170e8d9ec30843dd85da768ba75282dea63d70
cilikinari <cilikinari2005@gmail.com>
2026-07-15 14:07:21 +0800

    feat: implement routing with HomeScreen and ChatScreen components; add navigation to ChatScreen

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
-import HomeScreen from './screen/Homescreen'
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import HomeScreen from './screen/HomeScreen';
+import ChatScreen from './screen/ChatScreen';
 
 function App() {
   return (
-      <HomeScreen />
+    <BrowserRouter>
+      <Routes>
+        {/* Jika URL kosong (halaman awal), tampilkan HomeScreen */}
+        <Route path="/" element={<HomeScreen />} />
+        {/* Jika URL /chat, tampilkan ChatScreen */}
+        <Route path="/chat" element={<ChatScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
